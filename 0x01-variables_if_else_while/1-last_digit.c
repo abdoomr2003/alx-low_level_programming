7f68b677c81037e965fb1a80fa418579560580fe
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-07-15 01:47:36 +0300

    modifaing 1-last_digit.c file

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else if ((n%10)==0)
 	{
-		rintf("Last digit of %d is %d and is 0\n", n , n%10);
+		printf("Last digit of %d is %d and is 0\n", n , n%10);
 	}
 	else ((n%10)<6 && (n%10)!=0)
 	{
