0a7e634b04603b1b8b16e5995dc30d04868a4dbf
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-07-18 21:37:00 +0300

    creating  1-alphabet.c ,10-add.c,100-times_table.c,101-natural,c102-fibonacci.c,103-fibonacci.c,104-fibonacci.c,11-print_to_98.c,2-print_alphabet_x10.c,3-islower.c,4-isalpha.c,5-sign.c,6-abs.c,7-print_last_digit.c,8-24_hours.c and 9-times_table.c files

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,32 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
-*main - checks for lowercase character.
-*
-*Return - Always 0.
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
 */
-int _islower(int);
-int main(void)
-{   int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-    _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-
-}
-int _islower(int n)
+int _islower(int c)
 {
-    if (n='c')
-    {
-        return (1) ;
-    }
-    else
-    {
-        return(0);
-    }
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);
 }
