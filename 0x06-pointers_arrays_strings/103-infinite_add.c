906c06e2c2a23d9e241c9f306d3d0a5956a58aa9
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-07-27 20:59:35 +0300

    modefaid file name

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,68 +1,41 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: buffer size
  *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
- * rev_string - reverses a string
- * @s: the string to reverse
- *
- * Return: void
- */
-char *rev_string(char *s)
-{
-	int l = _strlen(s), i = 0;
-	char t;
-
-	for (i = 0; i < l / 2; i++)
-	{
-		t = s[l - i - 1];
-		s[l - i - 1] = s[i];
-		s[i] = t;
-	}
-	return (s);
-}
-
-/**
- * infinite_add - adds arbitrarily long string of digits
- * @n1: the first digit string
- * @n2: the second digit string
- * @r: the result buffer
- * @size_r: the size of result buffer
- *
- * Return: char pointer to buffer
+ * Return: pointer to the result
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
+    int i = 0, j = 0, k = 0, l = 0, m = 0;
+    char c;
 
-	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
-	{
-		if (i >= size_r)
-			return (0);
-		a = 0;
-		b = 0;
-		if (l1 >= 0)
-			a = n1[l1] - '0';
-		if (l2 >= 0)
-			b = n2[l2] - '0';
-		a = a + b + c;
-		c = a / 10;
-		a %= 10;
-		r[i] = a + '0';
-	}
-	r[i] = '\0';
-	return (rev_string(r));
+    while (n1[i] != '\0')
+        i++;
+    while (n2[j] != '\0')
+        j++;
+    if (i > size_r || j > size_r)
+        return (0);
+    for (i--, j--, k = 0; k < size_r - 1 && (i >= 0 || j >= 0); i--, j--, k++)
+    {
+        c = m;
+        if (i >= 0)
+            c += n1[i] - '0';
+        if (j >= 0)
+            c += n2[j] - '0';
+        r[k] = c % 10 + '0';
+        m = c / 10;
+    }
+    if (m != 0 || k == size_r - 1 || (k == size_r - 2 && (i >= 0 || j >= 0)))
+        return (0);
+    for (i = k - 1, l = 0; l < i; i--, l++)
+        c = r[i], r[i] = r[l], r[l] = c;
+    r[k] = '\0';
+    return (r);
 }
+
