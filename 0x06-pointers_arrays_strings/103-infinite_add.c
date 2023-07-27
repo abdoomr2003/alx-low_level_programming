a532532fda5a0f5c159bc2ae33d5d86e4e34262f
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-07-27 20:57:01 +0300

    modefaid file name

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,68 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * get_string_length - Calculate the length of a null-terminated string.
- * @str: The input string.
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: The length of the string.
+ * Return: integer length of string
  */
-int get_string_length(char *str)
+int _strlen(char *s)
 {
-    int length = 0;
-    while (str[length] != '\0')
-        length++;
-    return length;
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * add_digits - Add two digits along with a carry.
- * @d1: The first digit.
- * @d2: The second digit.
- * @carry: The carry value from the previous addition.
- * @result: A pointer to store the result of the addition (digit + carry).
+ * rev_string - reverses a string
+ * @s: the string to reverse
  *
- * Return: The carry value for the next addition.
+ * Return: void
  */
-int add_digits(char d1, char d2, int carry, char *result)
+char *rev_string(char *s)
 {
-    int sum = carry + (d1 - '0') + (d2 - '0');
-    *result = (sum % 10) + '0';
-    return sum / 10;
+	int l = _strlen(s), i = 0;
+	char t;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[l - i - 1];
+		s[l - i - 1] = s[i];
+		s[i] = t;
+	}
+	return (s);
 }
 
 /**
- * infinite_add - Add two numbers and store the result in a buffer.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
+ * infinite_add - adds arbitrarily long string of digits
+ * @n1: the first digit string
+ * @n2: the second digit string
+ * @r: the result buffer
+ * @size_r: the size of result buffer
  *
- * Return: A pointer to the result (in the buffer @r), or 0 if the result
- *         cannot be stored in the buffer.
+ * Return: char pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = get_string_length(n1);
-    int len2 = get_string_length(n2);
-    int carry = 0;
-    int sum;
-    int i = len1 - 1;
-    int j = len2 - 1;
-    int result_index = size_r - 1;
+	int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, a, b, c = 0;
 
-    // Check if the result can be stored in the buffer
-    if (len1 >= size_r || len2 >= size_r)
-        return 0;
-
-    // Perform the addition and store the result in the buffer
-    while (i >= 0 || j >= 0 || carry)
-    {
-        if (result_index - 1 < 0) 
-            return 0;
-
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i] - '0';
-        if (j >= 0)
-            sum += n2[j] - '0';
-
-        carry = add_digits('0', '0', sum, &r[result_index - 1]);
-
-        i--;
-        j--;
-        result_index--;
-    }
-
-    return r + result_index;
+	for (l1--, l2--, size_r--; l1 >= 0 || l2 >= 0 || c; l1--, l2--, i++)
+	{
+		if (i >= size_r)
+			return (0);
+		a = 0;
+		b = 0;
+		if (l1 >= 0)
+			a = n1[l1] - '0';
+		if (l2 >= 0)
+			b = n2[l2] - '0';
+		a = a + b + c;
+		c = a / 10;
+		a %= 10;
+		r[i] = a + '0';
+	}
+	r[i] = '\0';
+	return (rev_string(r));
 }
-
