3a388b2fc36d2eb5d7c73c9828f573b4e32f507f
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-07 15:41:35 +0300

    creating README.md and main.h files and doing first task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * create_array - creates an array of chars, and initializes it with a specific char.
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: char
+ */
+char *create_array(unsigned int size, char c);
+{
+	int* s = malloc(size_t size);
+	if(size==0)
+		return(NULL);
+	else
+		do
+		{
+			s[size] = c;
+			size--;
+		}while(size!=0);
+}
