c7de58c385ddb8f9ff4bc759c128ccbf45499fcb
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-08 00:28:17 +0300

    doing taskes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * str_concat - concatenate two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: pointer to the newly allocated concatenated string
+ * str_concat - Entry point
+ *@s1: string 1
+ *@s2: string 2
+ * Return: pointer should point to a newly allocated space in memory or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	int n1, n2, i, j = 0;
-	char *newstr;
+	char *strnew = NULL;
+	unsigned int i;
+	int n1;
+	int n2;
+	int count;
 
-	for (n1 = 0; s1[n1] != '\0'; n1++)
-		;
-	for (n2 = 0; s2[n2] != '\0'; n2++)
-		;
+	count = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	newstr = malloc((n1 + n2 + 1) * sizeof(char));
-	if (newstr == NULL)
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
 		return (NULL);
-	for (i = 0 ; s1[i] != '\0' ; i++)
-		newstr[i] = s1[i];
-	for (; s2[j] != '\0' ; i++, j++)
-		newstr[i] = s2[j];
-	newstr[i] = '\0';
-	return (newstr);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (; s2[count] != '\0'; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+	}
+	return (strnew);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,36 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * alloc_grid - create a 2D array of integers
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: pointer to the newly allocated 2D array (grid)
+ * alloc_grid - Entry point
+ *@width: rows of matrix
+ *@height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
-int **str_concat(int *width, int *height)
+int **alloc_grid(int width, int height)
 {
+	int **matrix;
 	int i;
-	int **grid = malloc(height * sizeof(int *));
+	int j;
+	int l;
+	int *p;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	if (grid == NULL)
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
 		return (NULL);
-	int h, w;
-
-	for (h = 0; h < height; h++)
+	for (i = 0; i < height; i++)
 	{
-		grid[h] = malloc(width * sizeof(int));
-		if (grid[h] == NULL)
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
+		if (*(matrix + i) == NULL)
 		{
-			for (i = 0; i < h; i++)
-				free(grid[i]);
-			free(grid);
+			for (i = 0; i < height; i++)
+			{
+				p = matrix[i];
+				free(p);
+			}
+			free(matrix);
 			return (NULL);
 		}
-		for (w = 0; w < width; w++)
-			grid[h][w] = 0;
 	}
-	return (grid);
+	for (l = 0; l < height; l++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[l][j] = 0;
+		}
+	}
+	return (matrix);
 }
