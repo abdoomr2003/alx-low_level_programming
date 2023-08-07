2b45985fa7b62bbd5e312f5c42406f8f0911f2c0
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-07 17:09:01 +0300

    modifaing failes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,12 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	char *s = malloc((size + 1) * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
