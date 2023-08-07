0e1d201fb517896d25543f07a3a51909291731d7
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-07 17:38:04 +0300

    modifaing failes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,35 +1,25 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
- * create_array - create an array of size with char c
+ * create_array - create an arry of size with char c
  * @size: unsigned int type
  * @c: char type
  * Return: Return pointer to array created
  */
 char *create_array(unsigned int size, char c)
 {
-    char *s = malloc(size * sizeof(char));
+	unsigned int i;
+	char *s = malloc((size + 1) * sizeof(char));
 
-    if (s == NULL)
-    {
-        // Failed to allocate memory, return NULL to indicate the failure
-        return NULL;
-    }
-
-    if (size == 0)
-    {
-        // Size is 0, return NULL to indicate an empty array
-        return NULL;
-    }
-
-    unsigned int i;
-    for (i = 0; i < size; i++)
-    {
-        s[i] = c;
-    }
-
-    return s;
+	if (size == 0)
+		return (NULL);
+	if (s == 0)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	s[size] = '\0';
+	return (s);
 }
-
