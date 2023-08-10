d1ca5593555b41446d844175ead6951a1fb629fc
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-10 12:22:58 +0300

    doing 0. Trust no one task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ *@b: size of the memory block to be allocated
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
