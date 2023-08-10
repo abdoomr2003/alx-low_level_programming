94ffffe62f4662acbcdbb7f1fcdc90ceec67f851
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-10 17:29:38 +0300

    modifaing

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- * Return: pointer to the resulting string
+ * string_nconcat - Main Entry
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, k;
 	char *s;
-	unsigned int i = 0, j = 0, size1 = 0, size2 = 0;
 
-	while (s1 && s1[size1])
-		size1++;
-	while (s2 && s2[size2])
-		size2++;
-
-	if (n < size2)
-		s = malloc(sizeof(char) * (size1 + n + 1));
+	if (s1 == NULL)
+		i = 0;
 	else
-		s = malloc(sizeof(char) * (size1 + size2 + 1));
-
-	if (!s)
-		return (NULL);
-
-	while (i < size1)
 	{
-		s[i] = s1[i];
-		i++;
+		for (i = 0; s1[i]; i++)
+			;
 	}
-
-	while (n < size2 && i < (size1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= size2 && i < (size1 + size2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
-
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
 	return (s);
 }
