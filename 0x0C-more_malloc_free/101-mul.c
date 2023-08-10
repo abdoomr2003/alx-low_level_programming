65e21f84b186a7e12792037330d4332b884ee7ad
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-10 14:00:02 +0300

    modifaing last task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,70 +1,110 @@
 #include "main.h"
-int _isdigit(char *argv);
-int _atoi(char *s);
-int main(int argc, char *argv[]);
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is a digit, otherwise 0.
+ */
+int _isdigit(char c)
 {
-	int i;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
+	return (c >= '0' && c <= '9');
 }
 /**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
+ * _strlen - Calculates the length of a string.
+ * @str: The input string.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *str)
 {
-	int i, result;
+	int len = 0;
 
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
+	while (str[len] != '\0')
+		len++;
+	return (len);
 }
 /**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
-int main(int argc, char *argv[])
+ * _multiply - Multiplies two positive numbers represented as strings.
+ * @num1: The first positive number.
+ * @num2: The second positive number.
+ */
+void _multiply(char *num1, char *num2)
 {
-	int i;
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int *result = malloc(sizeof(int) * (len1 + len2));
 
-	malloc();
-	if (argc != 3)
+	if (!result)
 	{
-		printf("Error\n");
+		_putchar("Error\n");
 		exit(98);
 	}
-	for (i = 1; i < argc; i++)
+	for (int i = 0; i < len1 + len2; i++)
+		result[i] = 0;
+
+	for (int i = len1 - 1; i >= 0; i--)
 	{
-		if (_isdigit(argv[i]))
+		for (int j = len2 - 1; j >= 0; j--)
 		{
-			printf("Error\n");
-			exit(98);
+			int product = (num1[i] - '0') * (num2[j] - '0');
+			int sum = product + result[i + j + 1];
+
+			result[i + j + 1] = sum % 10;
+			result[i + j] += sum / 10;
 		}
 	}
+	int printed = 0;
+
+	for (int i = 0; i < len1 + len2; i++)
+	{
+		if (result[i] != 0 || printed)
+		{
+			_putchar("%d", result[]);
+			printed = 1;
+		}
+	}
+	if (!printed)
+		_putchar("0");
+	_putchar("\n");
+	free(result);
+}
+/**
+ * main - multiply two positive numbers provided.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 if successful, 98 if there's an error.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		_putchar("Error\n");
+		return (98);
+	}
+
+	char *num1 = argv[1];
+	char *num2 = argv[2];
+
+	for (int i = 0; num1[i] != '\0'; i++)
+	{
+		if (!_isdigit(num1[i]))
+		{
+			_putchar("Error\n");
+			return (98);
+		}
+	}
+
+	for (int i = 0; num2[i] != '\0'; i++)
+	{
+		if (!_isdigit(num2[i]))
+		{
+			_putchar("Error\n");
+			return (98);
+		}
+	}
+	_multiply(num1, num2);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,9 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _isdigit(char *argv);
-int _atoi(char *s);
+int _isdigit(char c);
+int _strlen(char *str);
+void _multiply(char *num1, char *num2);
 int main(int argc, char *argv[]);
 
 #endif /*MAIN_H*/
