5825a1b0041c8acb277466218b7bf6de38171b15
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-16 13:20:11 +0300

    modifaing

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
 #include <stdlib.h>
 
@@ -7,4 +7,4 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /*_MAIN_H_ */
+#endif
