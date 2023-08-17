a1a6259b013f765bd49ab06fb76819ddc1c12f73
Abdelrahman_Atef <abdoomer1112003@gmail.com>
2023-08-17 15:51:54 +0300

    doing project

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: number of argument
+ *
+ * Return: sum
+ */
+int int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	int sum = 0;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, int);
+	}
+
+	va_end(valist);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,25 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdiarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct format - Data type of a format.
+ * @op: Format.
+ * @f: Function.
+ *
+ */
+
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
